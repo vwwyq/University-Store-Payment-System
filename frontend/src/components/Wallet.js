4445439da10ae9bad81ee6a3f3fad8950deaa910
Lewvy <aditya29december@gmail.com>
2025-03-13 11:39:27 +0530

    changes

--- frontend/src/components/Wallet.js
+++ frontend/src/components/Wallet.js
@@ -1,14 +1,3 @@
-
-const fetchWalletBalance = async () => {
-  const token = localStorage.getItem("jwt");
-  if (!token) return;
-
-  try {
-    const response = await axios.get("http://localhost:5000/wallet/balance", {
-      headers: { Authorization: `Bearer ${token}` },
-    })
-    console.log(response.data);
-  } catch (error) {
-    console.error("Failed to fetch wallet balance:", error);
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e0cdad7c5489623e70bf19b1af3d33ef05b637d62ef52c3532f36e617898af6d
+size 376
