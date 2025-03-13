13af8c2d0298f0818295e2138871405e429a79ea
Lewvy <aditya29december@gmail.com>
2025-03-13 12:26:10 +0530

    middleware authentication

---
+++ backend/middleware/authMid.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:69f052366e4d4cbd2857726a10d18e64fd2ced64f56e5c131f49c8ed75667693
+size 509
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b75522cd46d98effe0e95467fd893985e792de9613ebe767016aa80ee5f1107
-size 1669
+oid sha256:d901f62d870d2645a4e7c07f0eb902b4b2f585c72ee025c2e219c25f6008575d
+size 1357
