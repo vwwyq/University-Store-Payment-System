9d36a185c14fa3d3cf91397a404e95638571d055
totoro2629 <bidisha03.bg@gmail.com>
2025-03-28 18:02:05 +0530

    server and firebase connected with frontend

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,12 @@ admin.initializeApp({
 });
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
 app.use(express.json());
 
 app.use("/wallet", walletRoutes);
